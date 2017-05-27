69859d151963bcfed1c4f9ca8e9f0d4da19d996a
Jonas Kalderstam <jonas@kalderstam.se>
2017-05-27 13:08:48 +0200

    First roboelectric test

--- app/src/test/kotlin/com/nononsenseapps/feeder/ui/FeedActivityTest.kt
+++ app/src/test/kotlin/com/nononsenseapps/feeder/ui/FeedActivityTest.kt
@@ -2,23 +2,26 @@ package com.nononsenseapps.feeder.ui
 
 import com.nononsenseapps.feeder.BuildConfig
 import com.nononsenseapps.feeder.R
+import com.nononsenseapps.feeder.util.PrefUtils
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric.setupActivity
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import kotlin.test.fail
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class)
 class FeedActivityTest {
 
-
-
     @Test
-    fun clickingNightModeShouldToggleBackground() {
+    fun clickingNightModeShouldTogglePrefs() {
         val activity = setupActivity(FeedActivity::class.java)
+        assertFalse(PrefUtils.isNightMode(activity), "Should start with false")
         activity.findViewById(R.id.nightcheck).performClick()
-        fail("I did run it!")
+        assertTrue(PrefUtils.isNightMode(activity), "Should toggle value")
+        activity.findViewById(R.id.nightcheck).performClick()
+        assertFalse(PrefUtils.isNightMode(activity), "Should toggle value again")
     }
 }
