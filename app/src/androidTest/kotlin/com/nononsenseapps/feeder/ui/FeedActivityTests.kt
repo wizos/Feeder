d09fec3a9c07f263e814a72562c12b1dd7b64ba1
Jonas Kalderstam <jonas@kalderstam.se>
2017-08-19 12:22:23 +0200

    Fix emulator so espresso tests can run reliably

--- app/src/androidTest/kotlin/com/nononsenseapps/feeder/ui/FeedActivityTests.kt
+++ app/src/androidTest/kotlin/com/nononsenseapps/feeder/ui/FeedActivityTests.kt
@@ -2,6 +2,7 @@ package com.nononsenseapps.feeder.ui
 
 
 import android.support.test.InstrumentationRegistry.getInstrumentation
+import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import android.support.test.espresso.action.ViewActions.click
@@ -17,13 +18,11 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
 import com.nononsenseapps.feeder.R
-import org.hamcrest.Description
-import org.hamcrest.Matcher
+import com.rometools.rome.feed.synd.SyndFeed
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.Matchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -43,9 +42,9 @@ class FeedActivityTests {
         var checkedTextView = onView(allOf(withId(R.id.notifycheck), isNotChecked()))
 
         checkedTextView.perform(click())
-        Thread.sleep(100L)
+        Thread.sleep(500L)
 
-        checkedTextView = onView(allOf(withId(R.id.notifycheck), isChecked()))
+        checkedTextView = onView(allOf(withId(R.id.notifycheck)))
         checkedTextView.check(matches(allOf(isDisplayed(), isChecked())))
     }
 
@@ -59,8 +58,11 @@ class FeedActivityTests {
         urlEditText.perform(replaceText(url), closeSoftKeyboard())
         urlEditText.perform(pressImeActionButton())
 
-        val result = onView(childAtPosition(withId(R.id.results_listview), 0))
-        result.perform(click())
+        Thread.sleep(1000L)
+
+        onData(`is`(instanceOf(SyndFeed::class.java)))
+                .inAdapterView(withId(R.id.results_listview))
+                .perform(click())
 
         val tagEditText = onView(allOf(withId(R.id.feed_tag), isDisplayed()))
         tagEditText.perform(replaceText(tag), closeSoftKeyboard())
@@ -68,21 +70,5 @@ class FeedActivityTests {
 
         val addButton = onView(withId(R.id.add_button))
         addButton.perform(scrollTo(), click())
-    }
-
-    private fun childAtPosition(parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
