f1daf5a5b83cf964e90425816ccb424cbb3f337f
Jonas Kalderstam <jonas@kalderstam.se>
2018-02-18 12:24:18 +0100

    Added test for Slashdot article

--- app/src/test/kotlin/com/nononsenseapps/feeder/model/ArticleTextExtractorKtTest.kt
+++ app/src/test/kotlin/com/nononsenseapps/feeder/model/ArticleTextExtractorKtTest.kt
@@ -1,5 +1,6 @@
 package com.nononsenseapps.feeder.model
 
+import org.jsoup.Jsoup
 import org.junit.Test
 import java.io.InputStream
 import kotlin.test.assertFalse
@@ -27,6 +28,21 @@ class ArticleTextExtractorKtTest {
         }
         assertFalse("Comment should not have been included") {
             fullArticle!!.contains("<div class=\"article-comments\">")
+        }
+    }
+
+    @Test
+    fun testSlashdot() {
+        val fullArticle = extractArticleText(
+                getResourceAsStream("article_slashdot.html"),
+                "The Washington Post: Melissa Hobley, an executive at the dating app OkCupid, hears the complaints about the apps [being unable to find good matches] regularly and thinks they get a bad rap. Silicon Valley workers \"are in the business of scalable, quick solutions. And that's not what love is,\" Hobley said. \"You can't hurry love. It's reciprocal. You're not ordering an object. You're not getting a delivery in less than seven minutes.\" Finding love, she added, takes commitment and energy -- and, yes, time, no matter how inefficiently it's spent. \"You have a whole city obsessed with algorithms and data, and they like to say dating apps aren't solving the problem,\" Hobley said. \"But if a city is male-dominant, if a city is known for 16-hour work days, those are issues that dating apps can't solve.\" One thing distinguishes the Silicon Valley dating pool: The men-to-women ratio for employed, young singles in the San Jose metro area is higher than in any other major area. There were about 150 men for every 100 women, compared with about 125 to 100 nationwide, of never-married young people between 25 and 34 in San Jose, U.S. Census Bureau data from 2016 shows. That ratio permeates the economy here, all the way to the valley's biggest employers, which have struggled for years to bring more women into their ranks. Men make up about 70% of the workforces of "
+        )
+        val plain = Jsoup.parse(fullArticle).body().text().trim()
+        assertTrue("Should end with end of article") {
+            plain.endsWith("company filings show.")
+        }
+        assertTrue("Should start with start of article") {
+            plain.startsWith("The Washington Post:")
         }
     }
 
