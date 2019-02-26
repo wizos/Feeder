c5a474e05e6d5eec86accce684cae60fc7f1d700
Jonas Kalderstam <jonas@kalderstam.se>
2019-02-26 11:50:47 +0100

    Fixed night mode bug in side menu
    Webview would reset the night mode setting. Solution is to
    instantiate a webview before setting night mode.
    
    Fixes #172

--- app/src/main/java/com/nononsenseapps/feeder/FeederApplication.kt
+++ app/src/main/java/com/nononsenseapps/feeder/FeederApplication.kt
@@ -1,6 +1,7 @@
 package com.nononsenseapps.feeder
 
 import android.app.Application
+import android.webkit.WebView
 import org.conscrypt.Conscrypt
 import java.security.Security
 
@@ -9,5 +10,13 @@ class FeederApplication: Application() {
     init {
         // Install Conscrypt to handle missing SSL cyphers on older platforms
         Security.insertProviderAt(Conscrypt.newProvider(), 1)
+
+        // Instantiate a webview to avoid issue with webview resetting night mode
+        // See: https://stackoverflow.com/a/54191884/535073
+        try {
+            WebView(applicationContext)
+        } catch (_: Throwable) {
+            // Ignored
+        }
     }
 }
